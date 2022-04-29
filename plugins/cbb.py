cae4e4acb804e9c60993696cdb3957404415a73f
oming4 <setyawan564@gmail.com>
2022-04-29 20:46:11 +0800

    first commit

---
+++ plugins/cbb.py
@@ -0,0 +1,28 @@
+# (©)Codexbotz
+# Recode by @mrismanaziz
+# Modified by @SilenceSpe4ks
+# t.me/SharingUserbot X t.me/infobotmusik
+
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+
+from bot import Bot
+from config import CHANNEL, GROUP, OWNER
+
+
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+    if data == "about":
+        await query.message.edit_text(
+            text=f"<b>📚 ᴛᴇɴᴛᴀɴɢ ʙᴏᴛ ɪɴɪ 📚\n\n☕ ᴍᴀɴᴀɢᴇ ʙʏ : <a href='tg://settings'>ᴋʟɪᴋ ᴅɪsɪɴɪ</a></b>\n",
+            disable_web_page_preview = True,
+            reply_markup = InlineKeyboardMarkup(
+                [[InlineKeyboardButton("🔒 𝗧𝘂𝘁𝘂𝗽 🔒", callback_data="close")]]
+            ),
+        )
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except BaseException:
+            pass
